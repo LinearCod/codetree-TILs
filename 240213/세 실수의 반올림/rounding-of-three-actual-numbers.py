7331be546f5018434b49e3a99bae39774111f56a
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-13 03:09:30 +0900

    Title: 세 실수의 반올림, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/세 실수의 반올림/rounding-of-three-actual-numbers.py
+++ 240213/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -1,18 +1,4 @@
 a=float(input())
 b=float(input())
 c=float(input())
-i=str(round(a,3))
-j=str(round(b,3))
-k=str(round(c,3))
-if i[-1]=='0':
-    print(i+'00')
-else:
-    print(round(a,3))
-if j[-1]=='0':
-    print(j+'00')
-else:
-    print(round(b,3))
-if k[-1]=='0':
-    print(k+'00')
-else:
-    print(round(c,3))
+print(f"{a:.3f}\n{b:.3f}\n{c:.3f}")
