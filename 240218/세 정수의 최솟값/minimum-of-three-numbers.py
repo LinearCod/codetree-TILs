c495d82537e6682538fa181b2244266584cf6308
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-18 20:35:29 +0900

    Title: 세 정수의 최솟값, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240218/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -0,0 +1,13 @@
+a,b,c = map(int, input().split())
+if a <= b and a <= c:
+    print(a)
+else:
+    pass
+if b <= a and b <= c:
+    print(b)
+else:
+    pass
+if c <= a and c <= b:
+    print(c)
+else:
+    pass
