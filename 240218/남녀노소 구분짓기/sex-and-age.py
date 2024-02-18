e066b8028142602cac4dba14466055c95ae87aa2
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-18 21:24:18 +0900

    Title: 남녀노소 구분짓기, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240218/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+s = int(input())
+a = int(input())
+if a >= 19:
+    if s == 0:
+        print('MAN')
+    else:
+        print('WOMEN')
+else:
+    if s == 0:
+        print('BOY')
+    else:
+        print('GIRL')
