e71fabe5909cdd3c315cdbf47808445e8e224231
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-25 03:17:49 +0900

    Title: 369 게임, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/369 게임/369-game.py
+++ 240225/369 게임/369-game.py
@@ -6,10 +6,12 @@ for i in range(1,n+1):
         else:
             print(i, end=' ')
     if i >= 10 and i % 2 == 0:
-        if str(i)[0] == '3' or str(i)[1] == '3':
+        if str(i)[0] == '3' or str(i)[1] == '3' or str(i)[0] == '6' or str(i)[1] == '6' or str(i)[0] == '9' or str(i)[1] == '9' or i % 3 == 0:
             print('0', end=' ')
         else:
             print(i, end=' ')
     elif i >= 10 and i % 2 != 0:
-        if str(i)[0] == '3' or str(i)[1] == '3':
+        if str(i)[0] == '3' or str(i)[1] == '3' or str(i)[0] == '6' or str(i)[1] == '6' or str(i)[0] == '9' or str(i)[1] == '9' or i % 3 == 0:
             print('0', end=' ')
+        else:
+            print(i, end=' ')
