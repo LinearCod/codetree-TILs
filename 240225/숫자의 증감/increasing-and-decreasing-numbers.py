8b12ded146d85533d0f69b01736066695977e0f8
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-25 02:22:00 +0900

    Title: 숫자의 증감, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240225/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,9 @@
+inp = input().split()
+c = inp[0]
+n = int(inp[1])
+if c == 'A':
+    for i in range(1, n+1):
+        print(i, end=' ')
+elif c == 'D':
+    for i in range(n+1, 1, -1):
+        print(i, end=' ')
