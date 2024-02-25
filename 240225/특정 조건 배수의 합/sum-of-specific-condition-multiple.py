555255b371aba58408eda522a2f27457e1c198fc
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-25 23:52:59 +0900

    Title: 특정 조건 배수의 합, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240225/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,6 +1,11 @@
 a,b = map(int, input().split())
-c = 0
-for i in range(a,b+1):
-    if i % 5 == 0:
-        c += i
-print(c)
+c,d = 0,0
+if a < b:
+    for i in range(a,b+1):
+        if i % 5 == 0:
+            c += i
+elif a > b:
+    for i in range(b,a+1):
+        if i % 5 == 0:
+            d += i
+print(c+d)
