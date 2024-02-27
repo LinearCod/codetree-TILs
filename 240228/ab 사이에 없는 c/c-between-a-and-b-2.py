1c4e98753955fde4bf51e28e325c0ee3ce531339
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-28 02:03:08 +0900

    Title: ab 사이에 없는 c, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240228/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240228/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -3,8 +3,11 @@ a,b,c = map(int, input().split())
 booly = False
 
 for i in range(a,b+1):
-    if i % c != 0.000:
-        booly = True
+    if i % c != 0:
+        if type(i % c) == float:
+            pass
+        else:
+            booly = True
 
 if booly == True:
     print('YES')
