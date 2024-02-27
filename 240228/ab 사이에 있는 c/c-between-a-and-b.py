e8eec2ef2208443e98256876b973e6429db2d4db
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-28 01:33:35 +0900

    Title: ab 사이에 있는 c, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/ab 사이에 있는 c/c-between-a-and-b.py
+++ 240228/ab 사이에 있는 c/c-between-a-and-b.py
@@ -1,10 +1,12 @@
 a,b,c = map(int, input().split())
 
+booly = 0
+
 for i in range(a,b+1):
     if i % c == 0:
-        booly = '1'
+        booly = 1
         
-if booly == '1':
+if booly == 1:
     print('YES')
 else:
     print('NO')
