6fb790bae99b5049dd5bff672d7a2f57687918d4
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-28 00:46:25 +0900

    Title: 3n + 1 수열, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/3n + 1 수열/3n-plus-1-sequence.py
+++ 240228/3n + 1 수열/3n-plus-1-sequence.py
@@ -3,9 +3,10 @@ a = 0
 while True:
     if n % 2 == 0:
         n = n/2
-    elif n % 2 != 0 and n != 1:
-        n = 3*n+1
+    elif n % 2 != 0:
+        if n == 1:
+            print(a)
+            break
+        else:
+            n = 3*n+1
     a += 1
-    if n == 1:
-        print(a)
-        break
