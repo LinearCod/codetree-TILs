24b82c96881d98dc9f8be73ded6e37c5ed39ddaf
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-02-28 01:31:45 +0900

    Title: ab 사이에 있는 c, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240228/ab 사이에 있는 c/c-between-a-and-b.py
+++ 240228/ab 사이에 있는 c/c-between-a-and-b.py
@@ -1,15 +1,3 @@
-'''
-n = int(input())
-cnt = 0
-while n <= 1000:
-    if n % 2 == 0:
-        n = 3 * n + 1
-    elif n % 2 != 0:
-        n = 2 * n + 2
-    cnt += 1
-print(cnt)
-'''
-
 a,b,c = map(int, input().split())
 
 for i in range(a,b+1):
