800317c228e7cc9678a9a1026497183ede512858
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-06 22:37:56 +0900

    Title: 별표 출력하기 6, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,35 @@
+n = int(input())
+
+cnt = 0
+
+for i in range(n):
+    lit = []
+    
+    lit.append('* '*(-2*i+(2*n-1)))
+    
+    for j in range(cnt):
+        print('  ', end='')
+    
+    for j in lit:
+        print(j, end='')
+        
+    print()
+    
+    cnt += 1
+
+cnt = 2
+
+for i in range(n-1):
+    lit = []
+    
+    lit.append('* '*(2*i+3))
+    
+    for j in range(cnt):
+        print('  ', end='')
+        
+    for j in lit:
+        print(j, end='')
+        
+    print()
+    
+    cnt -= 1
