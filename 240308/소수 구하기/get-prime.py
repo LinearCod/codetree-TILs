eea5f1d3da0b8afdd2595d245ad6b7c9d2c82bed
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-08 01:25:44 +0900

    Title: 소수 구하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240308/소수 구하기/get-prime.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+cnt = 0
+lit = []
+
+for i in range(1, n+1):
+    cnt = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 2:
+        lit.append(i)
+
+for i in lit:
+    print(i, end=' ')
