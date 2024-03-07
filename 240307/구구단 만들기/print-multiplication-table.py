2300fc5810c76f0b1b919f76715279e7d17fa4c4
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-07 21:52:53 +0900

    Title: 구구단 만들기, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240307/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,19 @@
+a,b = map(int, input().split())
+
+if b - 2 != a:
+    lit = []
+    for i in range(b, a-1, -2):
+        lit.append(i)
+    for i in range(1, 10):
+        for j in lit:
+            print(f'{j} * {i} = {j*i}', end=' ')
+            if j > len(lit) - 1:
+                print('/', end=' ')
+        print()
+else:
+    for i in range(1, 10):
+        for j in range(b, a-1, -2):
+            print(f'{j} * {i} = {j*i}', end=' ')
+            if j != a:
+                print('/', end=' ')
+        print()
