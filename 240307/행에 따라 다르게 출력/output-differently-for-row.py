486717c39c0259b1ff6f9442f4e7b8c0502ff345
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-07 21:15:32 +0900

    Title: 행에 따라 다르게 출력, Time: 197ms, Memory: 26MB, Status: Passed - CodeTree

--- 240307/행에 따라 다르게 출력/output-differently-for-row.py
+++ 240307/행에 따라 다르게 출력/output-differently-for-row.py
@@ -1,12 +1,13 @@
 n = int(input())
 
+cnt = n + 2
+
 for i in range(n):
     for j in range(n):
         if i % 2 == 0:
             print(j + n*3*(i//2) + 1, end=' ')
+            if i != 0 and j == n - 1:
+                cnt = j + n*3*(i//2) + 1 + 2
         else:
-            if i != 1:
-                print(n + 2 + n*i + 2*j, end=' ')
-            else:
-                print(n + 2 + 2*j, end=' ')
+            print(cnt + 2*j, end=' ')
     print()
