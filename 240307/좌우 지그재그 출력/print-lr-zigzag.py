fc5bbbd6c4287bddb99399c036ceec528d864601
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-07 20:38:42 +0900

    Title: 좌우 지그재그 출력, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+cnt_1 = 1
+cnt_2 = n * 2
+
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            print(cnt_1, end=' ')
+            cnt_1 += 1
+            if j == n - 1:
+                cnt_1 += n
+        else:
+            print(cnt_2, end=' ')
+            cnt_2 -= 1
+            if j == n - 1:
+                cnt_2 = 8*(i + 1)
+    print()
