ce5e26bc815b9424b218df288b03992f8b922874
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-08 00:39:08 +0900

    Title: 3n + 1 수열 m번, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240308/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,14 @@
+m = int(input())
+
+cnt = 0
+
+for _ in range(m):
+    n = int(input())
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+            cnt += 1
+        else:
+            n = 3 * n + 1 
+            cnt += 1
+    print(cnt)
