b8de66003f785f80a0de9f8d9c5cf8c61f1319b0
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-10 01:28:51 +0900

    Title: 점수대 파악하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/점수대 파악하기/find-out-the-score-range.py
+++ 240310/점수대 파악하기/find-out-the-score-range.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 arr_1 = [0] * 10
 
 for i in arr:
-    if 100 > i > 10:
+    if 100 > i >= 10:
         t = int(str(i)[0])
         arr_1[-t] += 1
     elif i == 100:
