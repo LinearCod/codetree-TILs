b4da14c0346743a3668172347b3426fd7c08ce72
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-10 18:45:04 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 5078ms, Memory: 60MB, Status: Time Limit Exceed - CodeTree

--- 240310/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240310/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -2,17 +2,29 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-arr.sort()
+max_val = 0
 
-max_val = arr[-1]
+t = 7
 
-cnt = 0
+for i in arr:
+    if i > max_val:
+        max_val = i
 
-for i in arr[-1::-1]:
-    if max_val == i:
-        cnt += 1
+arr_count = arr.count(max_val)
 
-if cnt != 1:
-    print(-1)
-else:
-    print(max_val)
+while True:
+    if arr_count != 1 and t != 0:
+        for _ in range(arr_count):
+            if max_val not in arr:
+                t = 0
+                break
+            arr.remove(max_val)
+            if arr == []:
+                t = 0
+                break
+    elif t == 0:
+        print(-1)
+        break
+    elif arr_count == 1 and t != 0:
+        print(max(arr))
+    arr_count = arr.count(max(arr))
