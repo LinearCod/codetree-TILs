21fa37f5bc7c22082d4004042c5ddbeba295bdb2
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-10 16:06:58 +0900

    Title: 연속부분수열일까, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,34 @@
+n1,n2 = list(map(int, input().split()))
+
+arr_A = list(map(int, input().split()))
+
+arr_B = list(map(int, input().split()))
+
+leng_A = len(arr_A)
+
+leng_B = len(arr_B)
+
+cnt = arr_A.count(arr_A[0])
+
+start_a = []
+
+if cnt == 1:
+    idx = arr_A.index(arr_B[0])
+    New_A = arr_A[idx:idx + leng_B + 1]
+    if New_A == arr_B:
+        print('Yes')
+    else:
+        print('No')
+else:
+    for i in arr_A:
+        if i == arr_B[0]:
+            start_a.append(index(i)) 
+    for i in start_a:
+        if leng_B > len(arr_A[i:]):
+            break
+        else:
+            New_a = arr_A[i:i + leng_B + 1]
+            if New_a == arr_B:
+                print('Yes')
+            else:
+                print('No')
