c03d836fe727afc3dda53a275f2902129c9eeab7
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-10 14:45:33 +0900

    Title: 배열 놀이, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/배열 놀이/play-with-array.py
@@ -0,0 +1,19 @@
+n,q = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+for i in range(q):
+    question = list(map(int, input().split()))
+    type_q = question[0]
+    if type_q == 1:
+        print(arr[question[1] - 1])
+    elif type_q == 2:
+        cnt = arr.count(question[1])
+        if cnt == 0:
+            print(0)
+        else:
+            idx = arr.index(question[1])
+            print(idx + 1)
+    else:
+        for i in range(question[1], question[-1] + 1):
+            print(arr[i - 1], end=' ')
