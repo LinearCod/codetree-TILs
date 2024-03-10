0d3b26c05830111612f7232ea2a4f97b714b0cc4
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-10 19:19:43 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 415ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240310/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -6,15 +6,12 @@ max_val = max(arr)
 
 break_1 = 'N'
 
-while True:
+while arr != []:
     for i in arr:
         if arr == []:
-            break_1 = 'Y'
             break
         max_val = max(arr)
         if i == max_val:
             print(arr.index(i) + 1, end=' ')
             arr = arr[:arr.index(i)]
             max_val = 0
-    if break_1 == 'Y':
-        break
