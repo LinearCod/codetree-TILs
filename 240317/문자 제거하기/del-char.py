b0dcd1f5a4a958afbf19ac2d3454f98ec7d353fc
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 00:46:35 +0900

    Title: 문자 제거하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240317/문자 제거하기/del-char.py
+++ 240317/문자 제거하기/del-char.py
@@ -1,16 +1,27 @@
-word = input()
+# 문자열을 입력받습니다.
+string = input()
 
-arr = list(word)
+# 문자열의 길이를 구합니다.
+leng = len(string)
 
-t = len(arr)
+# pop 함수를 사용하기 위해 문자열을 list로 전환합니다.
+arr = list(string)
 
-while True:
-    n = int(input())
-    if len(arr) == 1:
-        break
-    if n > t:
-        arr.pop(-1)
-        print(''.join(arr))
-        continue
-    arr.pop(n)
-    print(''.join(arr))
+# 문자가 하나 남을 때까지 반복합니다.
+while leng > 1:
+	# 정수를 입력받습니다.
+	a = int(input())
+		
+	# 정수가 문자열의 길이 이상이면 마지막 문자를 가리키게 변경합니다.
+	if a >= leng:
+		a = leng - 1
+		
+	# a번 자리의 원소를 제거합니다.
+	arr.pop(a)
+	leng -= 1
+	
+	# list를 문자열로 변환합니다.
+	string = ''.join(arr)
+	
+	# 현재 문자열을 출력합니다.
+	print(string)
