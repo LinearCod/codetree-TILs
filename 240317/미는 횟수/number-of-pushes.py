6217e9f8fc83748ef3b252b78bacf3eb81546a2a
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 03:20:59 +0900

    Title: 미는 횟수, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240317/미는 횟수/number-of-pushes.py
+++ 240317/미는 횟수/number-of-pushes.py
@@ -8,15 +8,21 @@ if a == b:
     print(cnt)
 else:
     while True:
-        if cnt == 0:
+        if cnt == 0 and len(a) != 3:
             a_1 = a[1:] + a[0]
             cnt += 1
-        else:
+        elif len(a) == 3:
+            a_2 = a[-1] + a[0:2]
+            cnt += 1
+            if a_2 == b:
+                print(cnt)
+                break
+        elif cnt != 0 and len(a) != 3:
             a_1 = a_1[1:] + a_1[0]
+            cnt += 1
             if a_1 == a:
                 print(-1)
                 break
             if a_1 == b:
-                print(cnt + 1)
+                print(cnt)
                 break
-            cnt += 1
