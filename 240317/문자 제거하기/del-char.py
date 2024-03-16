b249cdcc031352fb16f308bb474d37ad94de7fdc
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 00:42:22 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240317/문자 제거하기/del-char.py
+++ 240317/문자 제거하기/del-char.py
@@ -4,11 +4,10 @@ arr = list(word)
 
 while True:
     n = int(input())
-    if len(arr) == 1:
+    if arr == []:
         break
     if n > len(arr):
         arr.pop(-1)
         print(''.join(arr))
-        break
     arr.pop(n)
     print(''.join(arr))
