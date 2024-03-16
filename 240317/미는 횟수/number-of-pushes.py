efbd7d39bdf224a5af0e12e08e50c8557ce3ee09
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 03:32:20 +0900

    Title: 미는 횟수, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240317/미는 횟수/number-of-pushes.py
+++ 240317/미는 횟수/number-of-pushes.py
@@ -1,26 +1,14 @@
-a = input()
+def min_shift_to_match(A, B):
+    n = len(A)
+    for shift in range(1, n + 1):
+        shifted_A = A[-shift:] + A[:-shift]  # 우측으로 shift 만큼 이동
+        if shifted_A == B:
+            return shift
+    return -1  # 문자열 A를 우측으로 밀어서 B와 같아지는 경우가 없는 경우
 
-b = input()
+# 입력
+A = input().strip()
+B = input().strip()
 
-cnt = 0
-
-if a == b:
-    print(cnt)
-else:
-    while True:
-        if cnt == 0:
-            a_1 = a[1:] + a[0]
-            b_1 = b[1:] + b[0]
-            cnt += 1
-        else:
-            if a_1 == b:
-                print(cnt)
-                break
-            if b_1 == a:
-                print(cnt)
-                break
-            if a_1 == a:
-                print(-1)
-                break
-            a_1 = a_1[1:] + a_1[0]
-            cnt += 1
+# 출력
+print(min_shift_to_match(A, B))
