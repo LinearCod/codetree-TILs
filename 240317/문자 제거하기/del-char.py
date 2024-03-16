7250590000361ce2d88552b621e751f635b4cb4c
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 00:40:37 +0900

    Title: 문자 제거하기, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240317/문자 제거하기/del-char.py
@@ -0,0 +1,14 @@
+word = input()
+
+arr = list(word)
+
+while True:
+    n = int(input())
+    if len(arr) == 1:
+        break
+    if n > len(arr):
+        arr.pop(1)
+        print(''.join(arr))
+        break
+    arr.pop(n)
+    print(''.join(arr))
