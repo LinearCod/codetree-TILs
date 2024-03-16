b232c68a9d4d52d4df7536bafea3e013ed7b3e6f
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 01:14:52 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240317/문자열 계속 지우기/keep-removing-string.py
+++ 240317/문자열 계속 지우기/keep-removing-string.py
@@ -1,19 +1,22 @@
 word_a = input()
 word_b = input()
 
-idx = word_a.index(word_b)
+if len(word_a) < len(word_b):
+    print(word_a)
+else:
+    idx = word_a.index(word_b)
 
-arr = list(word_a)
+    arr = list(word_a)
 
-while True:
-    if idx == -1:
-        print(''.join(arr))
-        break
-    elif arr == []:
-        break
-    else:
-        a = arr[0:idx]
-        b = arr[idx + len(word_b):]
-        arr = a + b
-        s = ''.join(arr)
-        idx = s.find(word_b)
+    while True:
+        if idx == -1:
+            print(''.join(arr))
+            break
+        elif arr == []:
+            break
+        else:
+            a = arr[0:idx]
+            b = arr[idx + len(word_b):]
+            arr = a + b
+            s = ''.join(arr)
+            idx = s.find(word_b)
