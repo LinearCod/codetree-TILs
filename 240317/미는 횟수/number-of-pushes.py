50a6c617edf1dc4cdb99f24f3c811232420526f7
LinearCod <157222589+LinearCod@users.noreply.github.com>
2024-03-17 03:27:55 +0900

    Title: 미는 횟수, Time: 5024ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240317/미는 횟수/number-of-pushes.py
+++ 240317/미는 횟수/number-of-pushes.py
@@ -19,18 +19,12 @@ else:
         #         print(cnt)
         #         break
         elif cnt != 0:
+            if b_1 == a:
+                print(cnt)
+                break
             if a_1 == b or b_1 == a:
                 print(cnt)
                 break
             a_1 = a_1[1:] + a_1[0]
             b_1 = b_1[1:] + b_1[0]
-            if a_1 == b or b_1 == a:
-                print(cnt)
-                break
             cnt += 1
-            if a_1 == a:
-                print(-1)
-                break
-            if a_1 == b or b_1 == a:
-                print(cnt)
-                break
